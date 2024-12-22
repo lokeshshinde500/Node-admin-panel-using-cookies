5817854cdd09fb5fd890d12810f6993a7503a116
lokesh146 <lokesh.shinde@swiftrut.com>
2024-12-23 04:09:07 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,14 @@
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1/adminData");
+
+mongoose.connect("mongodb+srv://lokeshshinde:qSGWIhhn6yxGnT9Z@cluster0.gu04j.mongodb.net/");
 
 const db = mongoose.connection.once("open", (error) => {
     if (error) {
         console.log("db not connected");
-    } else {
-        console.log("db connected :)");
-    }
+    }else{
+        console.log("db connected");
 })
+
 
 module.exports = db;
